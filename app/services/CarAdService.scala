af7ba885b4ca5aa024dd939067f3fb33e25958c0
Jaime Nuche <jaime.nuche@gmail.com>
2017-05-03 23:44:32 +0200

    Added dynamic table creation on startup

--- app/services/CarAdService.scala
+++ app/services/CarAdService.scala
@@ -1,23 +1,30 @@
 package services
 
 import scala.collection.JavaConversions.iterableAsScalaIterable
-
 import com.amazonaws.regions.Regions
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient
 import com.amazonaws.services.dynamodbv2.document.DynamoDB
 import com.amazonaws.services.dynamodbv2.document.Item
-
 import javax.inject.Singleton
 import models.CarAdFormats.carAdFormat
 import models.CarAdTypes.CarAd
 import models.CarAdTypes.Diesel
 import play.api.libs.json.Json
+import com.amazonaws.services.dynamodbv2.document.Table
+import scala.util.Try
+import scala.util.Success
+import scala.util.Failure
+import com.amazonaws.services.dynamodbv2.model.CreateTableRequest
+import com.amazonaws.services.dynamodbv2.model.KeySchemaElement
+import com.amazonaws.services.dynamodbv2.model.KeyType
+import com.amazonaws.services.dynamodbv2.model.AttributeDefinition
+import com.amazonaws.services.dynamodbv2.model.ProvisionedThroughput
 
 @Singleton
 class CarAdService {
   
   private final val dynamoClient: AmazonDynamoDBClient = new AmazonDynamoDBClient().withRegion(Regions.US_EAST_1)
-  private final val carAds = new DynamoDB(dynamoClient).getTable("CarAds")
+  private final val carAds = getCarAdsTable
 
   def createCarAd(carAd: CarAd) {
     carAds.putItem(Item.fromJSON(Json.toJson(carAd).toString))
@@ -40,6 +47,26 @@ class CarAdService {
     return carAd != null
   }
   
+  private def getCarAdsTable(): Table = {
+    Try(dynamoClient.describeTable(CarAdService.CAR_ADS_TABLE_NAME)) match {
+      case Success(_) => new DynamoDB(dynamoClient).getTable(CarAdService.CAR_ADS_TABLE_NAME)
+      case Failure(_) => createCarAdsTable
+    }
+  }
+  
+  private def createCarAdsTable(): Table = {
+    val primaryKey = new KeySchemaElement().withAttributeName("id").withKeyType(KeyType.HASH)
+    val attrsDef = new AttributeDefinition().withAttributeName("id").withAttributeType("N")
+    val throughput = new ProvisionedThroughput().withReadCapacityUnits(5L).withWriteCapacityUnits(5L)
+    val request = new CreateTableRequest().withTableName(CarAdService.CAR_ADS_TABLE_NAME)
+                                          .withKeySchema(primaryKey)
+                                          .withAttributeDefinitions(attrsDef)
+                                          .withProvisionedThroughput(throughput);
+    val table = new DynamoDB(dynamoClient).createTable(request)
+    table.waitForActive()
+    table
+  }
+  
   private def selectSortingFunction(sortField: String): (CarAd, CarAd) => Boolean = {
     (ad1: CarAd, ad2: CarAd) => {
       sortField match {
@@ -52,4 +79,8 @@ class CarAdService {
       }
     }
   }
+}
+
+object CarAdService {
+  private final val CAR_ADS_TABLE_NAME = "CarAds"
 }
